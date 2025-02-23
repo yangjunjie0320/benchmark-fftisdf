c1a7596674ba073e194da8b096c175663b8dbb4e
Junjie Yang <yangjunjie0320@gmail.com>
2025-02-23 14:05:46 -0800

    update

--- submit.py
+++ submit.py
@@ -1,6 +1,6 @@
 import os, sys, shutil
 
-def main(name=None, k0=None, c0=None, time="01:00:00", mem=200, ncpu=1):
+def main(name=None, time="01:00:00", mem=200, ncpu=1, cmd=None, scr=None):
     pwd = os.path.abspath(os.path.dirname(__file__))
 
     dir_name = os.path.join(pwd, "work/%s/c0-%.2f-k0-%.2f/" % (name, c0, k0))
@@ -9,8 +9,10 @@ def main(name=None, k0=None, c0=None, time="01:00:00", mem=200, ncpu=1):
         shutil.rmtree(dir_name)
     os.makedirs(dir_name)
 
-    os.system("cp %s/src/check-init-energy.py %s/main.py" % (pwd, dir_name))
-    # os.system("cp %s/src/run.sh %s/run.sh" % (pwd, dir_name))
+    assert cmd is not None
+    assert os.path.exists(scr)
+
+    os.system("cp %s %s/main.py" % (scr, dir_name))
 
     # add few lines to the beginning of the run.sh file
     lines = []
@@ -27,7 +29,7 @@ def main(name=None, k0=None, c0=None, time="01:00:00", mem=200, ncpu=1):
         f.write("export PREFIX=%s\n" % pwd)
         f.write("export DATA_PATH=$PREFIX/data/\n")
         f.write("export PYTHONPATH=$PREFIX/src/:$PYTHONPATH\n")
-        f.write("python main.py --name %s --k0 %6.2f --c0 %6.2f\n" % (name, k0, c0))
+        f.write(cmd + "\n")
 
     os.chdir(dir_name)
     os.system("sbatch run.sh")
@@ -37,16 +39,15 @@ if __name__ == "__main__":
     ncpu = 20
     mem = ncpu * 8
     
-    # for k0 in [20.0, 40.0, 60.0, 80.0]:
-    #     for c0 in [5.0, 10.0, 15.0, 20.0]:
-    #         main(name="diamond", k0=k0, c0=c0, time="04:00:00", mem=mem, ncpu=ncpu)
+    k0 = 60.0
+    c0 = 20.0
+    ke_cutoff = 10.0
 
-    # for k0 in [20.0, 40.0, 60.0, 80.0]:
-    #     for c0 in [10.0, 20.0, 30.0, 40.0]:
-    #         main(name="cco", k0=k0, c0=c0, time="04:00:00", mem=mem, ncpu=ncpu)
+    mm  = [[1, 1, 1], [1, 1, 2], [1, 2, 2], [2, 2, 2]]
+    mm += [[2, 2, 4], [2, 4, 4], [4, 4, 4]]
 
-    for k0 in [20.0, 40.0, 60.0, 80.0, 100.0]:
-        for c0 in [10.0, 20.0, 30.0, 40.0, 50.0]:
-            main(name="nio", k0=k0, c0=c0, time="04:00:00", mem=mem, ncpu=ncpu)
-
-
+    for kmesh in mm:
+        scr = "/home/junjiey/work/benchmark-fftisdf/src/check-vjk.py"
+        cmd = "python main.py --name=%s --k0=%6.2f --c0=%6.2f "
+        cmd += "--ke_cutoff=%6.2f --kmesh=%s" % (ke_cutoff, kmesh)
+        main(name="nio", k0=k0, c0=c0, time="04:00:00", mem=mem, ncpu=ncpu, cmd=cmd, scr=scr)
